4837140ef2e187fd9541e327840191cc7b1175f5
unknown <patrixstar565@gmail.com>
2021-03-12 13:23:55 +0100

    added shop page, created a responsive shop menu with items , collections and all about that. And added styles for shop page...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx'
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from "././pages/shoppage/shoppage.component.jsx";
+import { Route ,Switch } from 'react-router-dom';
+
+
+
 function App() {
   return (
     <div>
-        <HomePage/>
+        <Switch>
+          <HomePage exact path="/" component={HomePage} />
+          <ShopPage path="/shop" component={ShopPage} />
+        </Switch>
+        
     </div>
   );
 }
