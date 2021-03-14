0f07cd5cc3eb9f4b1c9a88925456dd103f96a1ab
unknown <patrixstar565@gmail.com>
2021-03-14 11:51:38 +0100

    Adding a header component with all the linkings and added styles to each components as well

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from "././pages/shoppage/shoppage.component.jsx";
 import { Route ,Switch } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 
 
 function App() {
   return (
     <div>
+      <Header/>
         <Switch>
           <HomePage exact path="/" component={HomePage} />
           <ShopPage path="/shop" component={ShopPage} />
