9a14c1ff05dc79cc65624c7fe18b4f9364fba031
unknown <patrixstar565@gmail.com>
2021-06-27 00:09:40 +0200

    Added quantity increaser of products in shopping basket

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
- 
+import {connect} from 'react-redux';
+import { addItem } from '../../redux/cart/cart.action'; 
 import "./collection-item.styles.scss"
-
-const CollectionItem = ({id, name, price, imageUrl}) => {
+import CustomButton from '../custom-button/custom-button.component';
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item;
     return ( 
         <div className="collection-item">
             <div className="image" style={{backgroundImage: `url(${imageUrl})`}}/>
@@ -10,8 +12,12 @@ const CollectionItem = ({id, name, price, imageUrl}) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton onClick={()=> {addItem(item)}} inverted> Add to Cart</CustomButton>
         </div>
      );
 }
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
  
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
