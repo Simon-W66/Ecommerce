c8ef4e380a67e4d6f2b75c25f303a84cdd9615db
unknown <patrixstar565@gmail.com>
2021-08-23 11:59:43 +0200

    Added dropdown list of items in basket

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,25 @@
+
 import React from 'react';
-import "./cart-dropdown.styles.scss"
+import { connect } from 'react-redux';
 
-import CustomButton from "../custom-button/custom-button.component"
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-const Dropdown = () => {
-    return ( 
-        <div className="cart-dropdown">
-            <div className="cart-items"></div>
-            <CustomButton> GO TO CHECKOUT</CustomButton>
-        </div>
-     );
-}
- 
-export default Dropdown;
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = ({ cartItems }) => (
+  <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import SHOP_DATA from "./shop.date.js"
+import SHOP_DATA from "./shop.data.js"
 import PreviewColection from "../../components/preview-collection/preview-collection.component"
 class ShopPage extends React.Component {
     constructor(props){
