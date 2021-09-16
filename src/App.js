df5d0d35c7045fbee7ba08e8a195dfa4dc8f8e04
unknown <patrixstar565@gmail.com>
2021-09-16 22:00:34 +0200

    Added checout page, automatical hidding basket whe move to checkot page...

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from "././pages/shoppage/shoppage.component.jsx";
 import { Route ,Switch, Redirect } from 'react-router-dom';
+import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions'
+import {selectCurrentUser} from "./redux/user/user.selectors"
+import {createStructuredSelector} from "reselect"
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -40,14 +43,15 @@ class App extends React.Component {
           <Switch>
             <HomePage exact path="/" component={HomePage} />
             <ShopPage path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={CheckoutPage}/>
             <SignInAndSignUpPage exact path="/signin" render={()=> this.props.currentUser ? (<Redirect to="/"/>) : (<SignInAndSignUpPage/>)} />
           </Switch>
       </div>
      );
   }
 }
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 }) 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -3,15 +3,19 @@ import "./cart-icon.styles.scss"
 import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg"
 import {toggleCartHidden} from "../../redux/cart/cart.action"
 import {connect} from "react-redux"
-
-const CartIcon = ({toggleCartHidden}) => (
+import {selectCartItemsCount} from "../../redux/cart/cart.selectors"
+import { createStructuredSelector } from 'reselect';
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon/>
-        <span className="item-counter">0</span>
+        <span className="item-counter">{itemCount}</span>
 
     </div>
 )
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () =>{ dispatch(toggleCartHidden())}
 })
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = createStructuredSelector({
+    itemCount : selectCartItemsCount
+})
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
