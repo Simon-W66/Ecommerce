ff8a496145dbe88f2857cec528651f85474e6081
unknown <patrixstar565@gmail.com>
2021-09-16 22:44:09 +0200

    increase and decrease quantity of products in checkout page

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,8 @@
+
+import CartItem from "../../components/cart-item/cart-item.component"
+import { clearItemFromCart } from "./cart.action"
 import CartActionType from "./cart.types"
-import {addItemToCart} from './cart.utils'
+import {addItemToCart, removeItemFromCart} from './cart.utils'
 const INITIAL_STATE = {
     hidden: true,
     cartItems: []
@@ -16,6 +19,16 @@ const cartReducer =(state = INITIAL_STATE, action) => {
             return{
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+        case CartActionType.CLEAR_ITEM_FROM_CART:
+            return{
+                ...state,
+                cartItems: state.cartItems.filter(CartItem => CartItem.id !== action.payload.id )
+            }
+        case CartActionType.REMOVE_ITEM:
+            return{
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
             }
         default:
             return state
